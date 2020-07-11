ff4fee2847cf69be2b39b9fb1a56c0bb17a47f68
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-07-11 15:30:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function addingEventListener() {
 
 addingEventListener();
 
-// IIF Immediately Invoked Function 
+// IIF Immediately Invoked Function
 // (function() {
 //   const input = document.getElementById('input');
 //   input.addEventListener('click', function(event) {
